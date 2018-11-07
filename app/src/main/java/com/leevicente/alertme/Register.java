63bbad6c0c50600843b8d778af136858b3b68ab2
ralph <2015081659@ust-ics.mygbiz.com>
2018-11-07 13:42:10 +0800

    fixed splash screen, page redirection

--- app/src/main/java/com/leevicente/alertme/Register.java
+++ app/src/main/java/com/leevicente/alertme/Register.java
@@ -1,7 +1,9 @@
 package com.leevicente.alertme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void buttonClickRegister(View v){
+        Intent i;
+        if (v.getId() == R.id.backLogin) {
+            i = new Intent(this, Login.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.nextPage) {
+            i = new Intent(this, Register2.class);
+            startActivity(i);
+        }
     }
 }
--- app/src/main/java/com/leevicente/alertme/Register2.java
+++ app/src/main/java/com/leevicente/alertme/Register2.java
@@ -1,28 +1,25 @@
 package com.leevicente.alertme;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class Register2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_register2);
     }
 
-    public void buttonClickLogin(View v){
+    public void buttonClickRegister2(View v){
         Intent i;
-        if (v.getId() == R.id.registerBtn) {
+        if (v.getId() == R.id.backPage1) {
             i = new Intent(this, Register.class);
             startActivity(i);
-        } else if (v.getId() == R.id.loginBtn) {
-            i = new Intent(this, Main.class);
+        } else if (v.getId() == R.id.nextpage2) {
+            i = new Intent(this, Register3.class);
             startActivity(i);
         }
     }
